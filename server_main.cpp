3dd4e4e290cdeb77a66ff377c4c4399c11f15115
laurent <laurent.thomas@open-cells.com>
2018-01-10 11:29:12 +0200

    initial

---
+++ server_main.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include "Server.h"
+#include <pthread.h>
+
+
+void* Work(void* user_v){
+	User* user = (User*)user_v;
+	Server::instance()->addUser(user);
+	Server::instance()->startConnection(user);
+	return NULL;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	pthread_t pid[100];
+	int cnt = 0;
+
+
+	cout << "Creating Server"<<endl;
+
+
+	Server* tcp = Server::instance();
+	tcp->createSocket(atoi(argv[1]));
+
+
+	while(true){
+
+		cout << " Accepting user\n";
+
+		User* new_user = tcp->acceptUser();
+
+		int err = pthread_create(&pid[cnt], NULL, &Work, new_user);
+		cnt++;
+
+	}
+
+	
+	return 0;
+}
